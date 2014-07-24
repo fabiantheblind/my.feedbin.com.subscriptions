c3c692e2a089c14dbdadebb3280465956763891a
fabiantheblind <icke@fabianmoronzirfas.me>
2014-07-24 10:19:42 +0200

    inital local commit

---
+++ parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+import codecs
+soup = BeautifulSoup(open("subscriptions.xml"))
+tags = soup.find_all("outline")
+f = codecs.open("links.md","w","utf-8")
+for tag in tags:
+    # print tag['htmlurl']
+    if tag.get('htmlurl') is None:
+        f.write("##" + tag.get('title')+ "\n")
+        # print "##" + tag.get('title')
+    else:
+        f.write("- ["+tag.get('text')+"]("+tag.get('htmlurl')+")  \n")
+        # print "- ["+tag.get('text')+"]("+tag.get('htmlurl')+")  "
+f.close()
